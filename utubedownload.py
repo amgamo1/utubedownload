deecb66718478b5855b17abfa63a3404acd3849d
amgamo1 <amgamo1@yahoo.com>
2022-07-21 15:27:45 +0100

    initial

--- utubedownload.py
+++ utubedownload.py
@@ -1,12 +1,15 @@
+from pytube import YouTube
 import pytube
-from pytube import Youtube
+from pytube.cli import on_progress
 
-try:
-    video_url ='https://www.youtube.com/watch?v=YXY1FhEWM88'
+video_url = 'https://www.youtube.com/watch?v=YXY1FhEWM88'
+max_res = '1080p'
+max_abr = '160kbps'
 
-    youtube = pytube.Youtube(video_url)
-    video = youtube.stream.first()
-    video.download('C:/Users/amgam/Videos/utubedownload')
-    print('Download processing')
-except:
-    print('Yo u fucked up')
+youtube = YouTube(video_url)
+print(youtube.streams)
+video = youtube.streams.get_by_itag('399')
+audio = youtube.streams.filter(abr = max_abr, progressive=False).first()
+video.download('C:/Users/amgam/Videos/utubedownload')
+audio.download('C:/Users/amgam/Videos/utubedownload')
+print('Download processing')
